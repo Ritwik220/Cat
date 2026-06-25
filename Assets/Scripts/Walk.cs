3b1affc37c97bebdeb66649959d56fce19e6c2e6
Ritwik Yadav <ritwikyadav35@gmail.com>
2026-06-25 22:19:42 +0530

    Imported a low poly environment, and gravity(needs work)

--- Assets/Scripts/Walk.cs
+++ Assets/Scripts/Walk.cs
@@ -6,22 +6,31 @@ public class Walk : MonoBehaviour
     private Animator animator;
     [SerializeField] float speed = 5.0f;
     [SerializeField] float turnSpeed = 5.0f;
+    private CharacterController controller;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        float gravity = 200f * Time.deltaTime;
+        controller.Move(new Vector3(0, -gravity, 0) * Time.deltaTime);
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
-        if(vertical != 0 || horizontal != 0)
+        transform.Rotate(new Vector3(0, horizontal, 0) * Time.deltaTime * turnSpeed);
+        if(vertical != 0)
         {
+            animator.SetBool("idle", false);
             animator.SetBool("walk", true);
-            transform.position += transform.forward * speed * Time.deltaTime * vertical;
-            transform.Rotate(new Vector3(0, horizontal, 0) * Time.deltaTime * turnSpeed);
+            Vector3 move = transform.forward * speed * Time.deltaTime * vertical;
+            controller.Move(move);
+            
+            
         }
         else
         {
